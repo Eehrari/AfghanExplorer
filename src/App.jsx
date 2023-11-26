20c8ffe240574b307daf2b1817d114a403970cc6
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2023-11-26 16:05:12 +0430

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import { useRef, useState } from 'react';
+
+import Places from './components/Places.jsx';
+import { AVAILABLE_PLACES } from './data.js';
+import Modal from './components/Modal.jsx';
+import DeleteConfirmation from './components/DeleteConfirmation.jsx';
+import logoImg from './assets/logo.png';
+
+function App() {
+  const modal = useRef();
+  const selectedPlace = useRef();
+  const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  function handleStartRemovePlace(id) {
+    modal.current.open();
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    modal.current.close();
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+  }
+
+  function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    modal.current.close();
+  }
+
+  return (
+    <>
+      <Modal ref={modal}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Afghan world" />
+        <h1>Afghan Explorer</h1>
+        <p>
+          Curate your exclusive collection of destinations you aspire to explore!
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to explore ..."
+          fallbackText={'Select the locations you would like to explore below.'}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Locations"
+          places={AVAILABLE_PLACES}
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,201 @@
+import mazarImg from './assets/mazarImg.jpg';
+import takharImg from './assets/takharImg.jpg';
+import helmandImg from './assets/helmandImg.jpg';
+import bamyanImg from './assets/bamyanImg.jpg';
+import nengarharImg from './assets/nengarharImg.jpg';
+import badghisImg from './assets/badghisImg.jpg';
+import kapisaImg from './assets/kapisaImg.jpg';
+import heratImg from './assets/heratImg.jpg';
+import kabulImg from './assets/kabulImg.jpg';
+import ghorImg from './assets/ghorImg.jpg';
+import kundozImg from './assets/kundozImg.jpg';
+import bandamirImg from './assets/bandamirImg.jpg';
+import parwanImg from './assets/parwanImg.jpg';
+import panjshirImg from './assets/panjshirImg.jpg';
+import laghmanImg from './assets/laghmanImg.jpg';
+import kandaharImg from './assets/kandaharImg.jpg';
+import zabulImg from './assets/zabulImg.jpg';
+import nuristanImg from './assets/nuristanImg.jpg';
+
+export const AVAILABLE_PLACES = [
+  {
+    id: 'm1',
+    title: 'Mazar Sharif',
+    image: {
+      src: mazarImg,
+      alt: 'Home to the historic city of Mazar-i-Sharif and a center of ancient culture and trade.',
+    },
+    lat: 44.5588,
+    lon: -80.344,
+  },
+  {
+    id: 'm2',
+    title: 'Takhar',
+    image: {
+      src: takharImg,
+      alt: 'Known for its fertile lands and being a center for trade and agriculture.',
+    },
+    lat: 25.0,
+    lon: 0.0,
+  },
+  {
+    id: 'm3',
+    title: 'Helmand',
+    image: {
+      src: helmandImg,
+      alt: 'Primarily an agricultural province, known for its production of opium poppies.',
+    },
+    lat: 27.9881,
+    lon: 86.925,
+  },
+  {
+    id: 'm4',
+    title: 'Bamyan',
+    image: {
+      src: bamyanImg,
+      alt: 'Renowned for the ancient Buddha statues carved into cliffs and its scenic valleys.',
+    },
+    lat: 18.2208,
+    lon: -66.5901,
+  },
+  {
+    id: 'm5',
+    title: 'Nangarhar',
+    image: {
+      src: nengarharImg,
+      alt: 'A region of diverse landscapes and agricultural productivity, particularly in fruits.',
+    },
+    lat: 37.9715,
+    lon: 23.7257,
+  },
+  {
+    id: 'm6',
+    title: 'Badghis',
+    image: {
+      src: badghisImg,
+      alt: 'Characterized by its agricultural significance, especially in cotton production.',
+    },
+    lat: -3.4653,
+    lon: -62.2159,
+  },
+  {
+    id: 'm7',
+    title: 'Kapisa',
+    image: {
+      src: kapisaImg,
+      alt: 'Known for its natural beauty and strategic location near the capital, Kabul.',
+    },
+    lat: 64.9631,
+    lon: -19.0208,
+  },
+  {
+    id: 'm8',
+    title: 'Herat',
+    image: {
+      src: heratImg,
+      alt: 'A major cultural and economic hub, renowned for its historic sites and vibrant culture.',
+    },
+    lat: 34.9949,
+    lon: 135.785,
+  },
+  {
+    id: 'm9',
+    title: 'Kabul',
+    image: {
+      src: kabulImg,
+      alt: 'The capital province, housing the country\'\s capital city and a melting pot of cultures.',
+    },
+    lat: -18.2871,
+    lon: 147.6992,
+  },
+  {
+    id: 'm10',
+    title: 'Ghor',
+    image: {
+      src: ghorImg,
+      alt: 'Famous for its rugged terrain and historical sites dating back to ancient times.',
+    },
+    lat: 48.8566,
+    lon: 2.3522,
+  },
+  {
+    id: 'm11',
+    title: 'Kundoz',
+    image: {
+      src: kundozImg,
+      alt: 'Important for agriculture and trade, with a mix of diverse ethnic groups.',
+    },
+    lat: 36.1069,
+    lon: -112.1129,
+  },
+  {
+    id: 'm12',
+    title: 'Band-e-Amir',
+    image: {
+      src: bandamirImg,
+      alt: 'Known for its rugged landscapes and being a region of diverse ethnic groups.',
+    },
+    lat: 45.4408,
+    lon: 12.3155,
+  },
+  {
+    id: 'm13',
+    title: 'Parwan',
+    image: {
+      src: parwanImg,
+      alt: 'Known for its historical sites and being home to the ancient city of Charikar.',
+    },
+    lat: 27.1751,
+    lon: 78.0421,
+  },
+  {
+    id: 'm14',
+    title: 'Panjshir',
+    image: {
+      src: panjshirImg,
+      alt: 'Notable for its stunning natural beauty, especially the Panjshir Valley.',
+    },
+    lat: 9.4981,
+    lon: 76.3388,
+  },
+  {
+    id: 'm15',
+    title: 'Laghman',
+    image: {
+      src: laghmanImg,
+      alt: 'Known for its fertile valleys and historical significance dating back centuries.',
+    },
+    lat: -2.153,
+    lon: 34.6857,
+  },
+  {
+    id: 'm16',
+    title: 'Kandahar',
+    image: {
+      src: kandaharImg,
+      alt: 'Historically significant and culturally rich, with nice people.',
+    },
+    lat: -17.9243,
+    lon: 25.8572,
+  },
+  {
+    id: 'm17',
+    title: 'Zabul',
+    image: {
+      src: zabulImg,
+      alt: 'Noted for its mountainous terrain and being a region of Pashtun tribes.',
+    },
+    lat: -13.1631,
+    lon: -72.545,
+  },
+  {
+    id: 'm18',
+    title: 'Nuristan',
+    image: {
+      src: nuristanImg,
+      alt: 'Famed for its remote and picturesque valleys, also known for its unique culture.',
+    },
+    lat: -3.4653,
+    lon: -58.38,
+  },
+];
